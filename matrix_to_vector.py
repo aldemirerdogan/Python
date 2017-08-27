eef07df4c56dd3a6804e67cecccfa08df5ee91b0
erdogan <aldemir.erdogan@gmail.com>
2017-08-27 23:34:49 +0300

    matplot library and image acquisition

--- matrix_to_vector.py
+++ matrix_to_vector.py
@@ -1,8 +1,15 @@
+# import libraries for image acquisition
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
-import numpy as np
 
 # Load an color image in grayscale d
-img = mpimg.imread('lena.jpg')
-imgplot = plt.imshow(img)
-plt.imshow(imgplot)
+img = mpimg.imread('tulip.jpg')
+
+# plot the data in RGB
+image_plot = plt.imshow(img)
+
+# Select a color from RGB
+lum_img = img[:, :, 2]
+
+# Plot the blue matrix
+image_plot_2 = plt.imshow(lum_img)
