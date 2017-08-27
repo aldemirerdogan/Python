d236ee5c45ea25778647006f2fd0122c3c0d3b9e
erdogan <aldemir.erdogan@gmail.com>
2017-08-27 22:27:26 +0300

    test for simple algorithms on Python language added comment lines

--- factorial.py
+++ factorial.py
@@ -1,13 +1,17 @@
 # simple factorial function
+
+# Calculate factorial of the input_fac
 input_fac = 6
 
 
+# Define the factorial function
 def factorial(number):
     result_fac = 1
     for i in range(number):
         result_fac = (number - i) * result_fac
     return result_fac
 
+# Call the factorial function
 r = factorial(input_fac)
 
 print(r)
